83bb246620b0d6d9f3e7cf0e9b2ad104eb6a5b15
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-07 21:38:03 +0100

    StudentController class is created and GetAllStudents method is added to it.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models;
+using HogwartsPotions.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers
+{
+    [ApiController, Route("/student")]
+    public class StudentController : ControllerBase
+    {
+        private readonly HogwartsContext _context;
+
+        public StudentController(HogwartsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<Student>> GetAllStudents()
+        {
+            return await _context.GetAllStudents();
+        }
+
+    }
+}
