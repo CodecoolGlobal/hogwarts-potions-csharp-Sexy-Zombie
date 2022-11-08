94c17b4774517fe6a7a37047ec008fbf3a3d1c81
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-08 21:14:16 +0100

    A few new items/values are added to DbInitializer.

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using HogwartsPotions.Models.Enums;
+using System.Xml.Linq;
 
 namespace HogwartsPotions.Data
 {
@@ -20,6 +21,27 @@ namespace HogwartsPotions.Data
                 return;   // DB has been seeded
             }
 
+            /*var ingredients = new Ingredient[]
+            {
+                new Ingredient { Name = "MandragoraRoot" },
+                new Ingredient { Name = "OldMansBeard" },
+                new Ingredient { Name = "TearDrop" },
+                new Ingredient { Name = "Salt" },
+                new Ingredient { Name = "ChickenEgg" },
+                new Ingredient { Name = "SunFlower" },
+                new Ingredient { Name = "Mud" },
+                new Ingredient { Name = "Sugar" }
+            };
+
+            foreach (Ingredient c in ingredients)
+            {
+                context.Ingredients.Add(c);
+            }
+            context.SaveChanges();
+
+
+
+
             var students = new Student[]
             {
             new Student{Name= "Carson",HouseType= HouseType.Gryffindor,PetType= PetType.Owl, Room = new Room()},
@@ -32,19 +54,77 @@ namespace HogwartsPotions.Data
             }
             context.SaveChanges();
 
-            var courses = new Room[]
+            var rooms = new Room[]
             {
             new Room{Capacity= 3, Residents = new HashSet<Student>()},
 
             new Room{Capacity= 2, Residents = new HashSet<Student>()}
 
             };
-            foreach (Room c in courses)
+            foreach (Room c in rooms)
             {
                 context.Rooms.Add(c);
             }
+            context.SaveChanges();*/
+
+
+            var recipes = new Recipe[]
+            {
+                new Recipe{ Name = "BigBumm", Student = new Student{Name= "Donald",HouseType= HouseType.Gryffindor,PetType= PetType.Owl, Room = new Room()}, Ingredients = new List<Ingredient>()
+                    {new Ingredient{Name = "Bean"}, new Ingredient{Name = "Pepper"}, new Ingredient{Name = "Cocoa"}}}
+
+            };
+
+            foreach (Recipe c in recipes)
+            {
+                context.Recipes.Add(c);
+            }
             context.SaveChanges();
-            
+
+
+            /*
+            var potions = new Potion[]
+            {
+                new Potion
+                {
+                    Name = "BigBumm", Ingredients = new List<Ingredient>
+                    {
+                        new Ingredient { Name = "MandragoraRoot" },
+                        new Ingredient { Name = "OldMansBeard" },
+                        new Ingredient { Name = "TearDrop" }
+                    },
+                    Recipe = new Recipe
+                    {
+                        Name = "BadaBumm",
+                        Student = new Student
+                        {
+                            Name = "Kelvin", HouseType = HouseType.Slytherin, PetType = PetType.Rat,
+                            Room = new Room { Capacity = 4 }
+                        },
+                        Ingredients = new List<Ingredient>
+                        {
+                            new Ingredient { Name = "MandragoraRoot" },
+                            new Ingredient { Name = "OldMansBeard" },
+                            new Ingredient { Name = "TearDrop" },
+                            new Ingredient { Name = "Salt" }
+                        }
+                    }, Student = new Student
+                    {
+                        Name = "John", HouseType = HouseType.Slytherin, PetType = PetType.Rat,
+                        Room = new Room { Capacity = 3 }
+                    }
+                }
+            };
+            foreach (Potion c in potions)
+            {
+                context.Potions.Add(c);
+            }
+            context.SaveChanges();
+
+            */
+
+
+
         }
     }
 }
