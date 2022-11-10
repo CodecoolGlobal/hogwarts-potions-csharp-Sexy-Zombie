5ddba2d6a424bf46ec02c895c4bed43616b295ad
Sexy-Zombie <szilgyigbor@gmail.com>
2022-11-10 11:00:55 +0100

    Newtonsoft is added to startup configuration file.

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,9 @@ namespace HogwartsPotions
             services.AddDbContext<HogwartsContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
